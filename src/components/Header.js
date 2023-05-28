d078873803fa8ef27ad5ea2eb96d631f53bf15f6
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-28 13:30:07 -0300

    chore: add new logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 export function Header() {
     return (
-        <header className="fixed top-0 z-20 flex justify-start align-center md:justify-center bg-black opacity-70 w-full h-24 text-white" >
-            <Image className="h-15 pl-3 md:pl-0" src={"/explorer-logo.svg"} alt="logo" width={250} height={300}/>
+        <header className="shadow-2xl fixed top-0 z-10 flex justify-start align-center md:justify-center bg-black opacity-80  w-full h-24 text-hsl(0, 0%, 100%)" >
+            <Image className=" pl-3 md:pl-0" src={"/logo-3.svg"} alt="logo" width={300} height={300}/>
         </header>)
 }
