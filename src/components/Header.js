c8491e4250abe40dea22a1f81d25feb0d843f525
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-05-29 14:34:55 -0300

    refactor: change some header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import Image from "next/image"
-export function Header() {
-    return (
-        <header className="shadow-2xl fixed top-0 z-10 flex justify-start align-center md:justify-center bg-black opacity-80  w-full h-24 text-hsl(0, 0%, 100%)" >
-            <Image className=" pl-3 md:pl-0" src={"/logo-3.svg"} alt="logo" width={300} height={300}/>
-        </header>)
+export function Header({params}) {
+    return (<header className="shadow-2xl fixed top-0 z-10 flex justify-start align-center md:justify-center bg-black opacity-80  w-full h-24 text-hsl(0, 0%, 100%)" >
+        <Image className=" pl-3 md:pl-0" src={"/logo-3.svg"} alt="logo" width={300} height={300} />
+    </header>)
 }
